514865e07bef26044d971bebc62c972a6019f030
al feratovic <aladin.feratovic@gmail.com>
2020-08-23 23:52:44 -0400

    changed portcc

---
+++ client/build/precache-manifest.008e37b3c1f1419fbf4143be900bb2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a68736f776d134349c21ca163dff44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912c9d6ad026e4cd6154",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "d0ea2ec5f269e24d8609",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "912c9d6ad026e4cd6154",
+    "url": "/static/js/2.a36a2b7c.chunk.js"
+  },
+  {
+    "revision": "d0ea2ec5f269e24d8609",
+    "url": "/static/js/main.eecce12d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
